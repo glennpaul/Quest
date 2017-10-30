4fa3bc0ad06e61e65609fb63c5e49fb6773450c5
Paul <glenn.paul@live.ca>
2017-10-29 22:07:33 -0400

    Corrected functionality for adding quests

--- app/src/main/java/com/example/paul/quest/Home.java
+++ app/src/main/java/com/example/paul/quest/Home.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -21,6 +22,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
@@ -32,15 +34,17 @@ public class Home extends AppCompatActivity {
     private String userID;
     private ArrayAdapter<String> arrayAdapter;
     private TextView header;
+    private EditText newQuestText;
     private List<String> quests;
     private DatabaseReference myRef = FirebaseDatabase.getInstance().getReference(); //get reference for root of database
+    private String questListID,quest_count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
 
-        ListView quest_list  = findViewById(R.id.quest_list);
+        final ListView quest_list  = findViewById(R.id.quest_list);
         quests = new ArrayList<>();
         arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, quests);
         quest_list.setAdapter(arrayAdapter);
@@ -49,6 +53,7 @@ public class Home extends AppCompatActivity {
         header = findViewById(R.id.Quest_header);
         Button sign_out = findViewById(R.id.sign_out);
         Button add_quest_btn = findViewById(R.id.addQuestbtn);
+        newQuestText = findViewById(R.id.etAddQuest);
 
         //listener for sign out button, if clicked, sign out of account and go back to login screen
         sign_out.setOnClickListener(new View.OnClickListener() {
@@ -63,7 +68,9 @@ public class Home extends AppCompatActivity {
         add_quest_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                addToQuestList("hi");
+                quest_count = String.valueOf(Integer.parseInt(quest_count)+1);
+                myRef.child("QuestList").child(questListID).child(quest_count).setValue(newQuestText.getText().toString());
+                myRef.child("QuestList").child(questListID).child("Count").setValue(quest_count);
             }
         });
     }
@@ -83,11 +90,13 @@ public class Home extends AppCompatActivity {
         myRef.child("QuestList").orderByChild("ID").equalTo(userID).addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                questListID = dataSnapshot.getKey();
                 for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
                     if(!postSnapshot.getKey().equals("ID") & !postSnapshot.getKey().equals("Count") & postSnapshot.getValue() != null) {
                         addToQuestList(postSnapshot.getValue().toString());
                     } else if (postSnapshot.getKey().equals("Count")){
-                        String quest_count_header = "Quests: " + postSnapshot.getValue().toString();
+                        quest_count = postSnapshot.getValue().toString();
+                        String quest_count_header = "Quests: " + quest_count;
                         header.setText(quest_count_header);
                     }
                 }
@@ -97,8 +106,12 @@ public class Home extends AppCompatActivity {
                 quests.clear();
                 arrayAdapter.notifyDataSetChanged();
                 for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
-                    if(!postSnapshot.getKey().equals("ID") & postSnapshot.getValue()!=null) {
+                    if(!postSnapshot.getKey().equals("ID") & !postSnapshot.getKey().equals("Count") & postSnapshot.getValue() != null) {
                         addToQuestList(postSnapshot.getValue().toString());
+                    } else if (postSnapshot.getKey().equals("Count")){
+                        quest_count = postSnapshot.getValue().toString();
+                        String quest_count_header = "Quests: " + quest_count;
+                        header.setText(quest_count_header);
                     }
                 }
             }
